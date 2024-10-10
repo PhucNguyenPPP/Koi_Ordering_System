942abba5f4755fe30f8b6edeb3f864ef5455454f
huyqu <huyquac@gmail.com>
2024-10-10 22:37:41 +0700

    feat: add api policy management

--- Service/Interfaces/IPolicyService.cs
+++ Service/Interfaces/IPolicyService.cs
@@ -3,6 +3,6 @@ public interface IPolicyService
     Task<List<PolicyDTO>> GetAllPoliciesAsync();
     Task<PolicyDTO> GetPolicyByIdAsync(Guid policyId);
     Task<bool> AddPolicyAsync(PolicyDTO policyDTO);
-    Task<bool> UpdatePolicyAsync(PolicyDTO policyDTO);
+    Task<bool> UpdatePolicyAsync(Guid policyId, PolicyDTO policyDTO);
     Task<bool> DeletePolicyAsync(Guid policyId);
 }
