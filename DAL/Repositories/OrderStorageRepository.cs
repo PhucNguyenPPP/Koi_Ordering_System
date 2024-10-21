46b13354421806370a1d75d4c1d743ce4865015a
huyqu <huyquac@gmail.com>
2024-10-22 00:04:19 +0700

    feat: add get all order asigned shipper api

--- DAL/Repositories/OrderStorageRepository.cs
+++ DAL/Repositories/OrderStorageRepository.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DAL.Entities;
 using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories
 {
@@ -12,6 +13,30 @@ namespace DAL.Repositories
     {
         public OrderStorageRepository(KoiDbContext context) : base(context)
         {
+        }
+        public async Task<IEnumerable<OrderShipperDTO>> GetAssignedOrdersByShipper(Guid shipperId)
+        {
+            var assignedOrders = await _dbSet
+                .Where(os => os.ShipperId == shipperId)
+                .Include(os => os.Order)
+                .ThenInclude(o => o.Customer)
+                .Include(os => os.Order)
+                .ThenInclude(o => o.Kois) // Assuming you want the farm data through Kois
+                .Select(os => new OrderShipperDTO
+                {
+                    OrderId = os.Order.OrderId,
+                    OrderNumber = os.Order.OrderNumber,
+                    FarmId = os.Order.Kois.FirstOrDefault().FarmId, // Assuming relation through Koi
+                    FarmName = os.Order.Kois.FirstOrDefault().Farm.FarmName,
+                    CreatedDate = os.Order.CreatedDate,
+                    TotalPrice = os.Order.TotalPrice,
+                    Status = os.Order.Status,
+                    CustomerId = os.Order.CustomerId,
+                    CustomerName = os.Order.Customer.FullName
+                })
+                .ToListAsync();
+
+            return assignedOrders;
         }
     }
 }
