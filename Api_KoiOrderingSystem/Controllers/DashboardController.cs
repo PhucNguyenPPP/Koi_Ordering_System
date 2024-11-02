87e1ccd87e4381d714f77d58b21be68db1379ddd
nghthtam <thanhtamthd3@gmail.com>
2024-11-03 03:08:55 +0700

    [Add]_Function_GetProfitByAdmin [Add]_Function_GetRevenueByFarm [Add]_Function_GetRevenueByAdmin

--- Api_KoiOrderingSystem/Controllers/DashboardController.cs
+++ Api_KoiOrderingSystem/Controllers/DashboardController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Common.DTO.General;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service.Interfaces;
 
 namespace Api_KoiOrderingSystem.Controllers
 {
@@ -7,5 +10,69 @@ namespace Api_KoiOrderingSystem.Controllers
     [ApiController]
     public class DashboardController : ControllerBase
     {
+        private readonly IDashboardService _dashboardService;
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+        [HttpGet("revenue-admin")]
+        public async Task<IActionResult> GetRevenueByAdmin([Required] DateOnly startdate,
+                                                           [Required] DateOnly enddate)
+        {
+            ResponseDTO responseDTO = await _dashboardService.GetRevenueByAdmin(startdate, enddate);
+            if (responseDTO.IsSuccess == false)
+            {
+                if (responseDTO.StatusCode == 404)
+                {
+                    return NotFound(responseDTO);
+                }
+                else
+                {
+                    return BadRequest(responseDTO);
+                }
+            }
+
+            return Ok(responseDTO);
+        }
+
+        [HttpGet("revenue-farm")]
+        public async Task<IActionResult> GetRevenueByFarm([Required] DateOnly startdate,
+                                                          [Required] DateOnly enddate,
+                                                          [Required] Guid farmId)
+        {
+            ResponseDTO responseDTO = await _dashboardService.GetRevenueByFarm(startdate, enddate, farmId);
+            if (responseDTO.IsSuccess == false)
+            {
+                if (responseDTO.StatusCode == 404)
+                {
+                    return NotFound(responseDTO);
+                }
+                else
+                {
+                    return BadRequest(responseDTO);
+                }
+            }
+
+            return Ok(responseDTO);
+        }
+        [HttpGet("profit")]
+        public async Task<IActionResult> GetProfitByAdmin([Required] DateOnly startdate,
+                                                          [Required] DateOnly enddate)
+        {
+            ResponseDTO responseDTO = await _dashboardService.GetProfitByAdmin(startdate, enddate);
+            if (responseDTO.IsSuccess == false)
+            {
+                if (responseDTO.StatusCode == 404)
+                {
+                    return NotFound(responseDTO);
+                }
+                else
+                {
+                    return BadRequest(responseDTO);
+                }
+            }
+
+            return Ok(responseDTO);
+        }
     }
 }
--- Service/Interfaces/IDashboardService.cs
+++ Service/Interfaces/IDashboardService.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.DTO.General;
 
 namespace Service.Interfaces
 {
     public interface IDashboardService
     {
+        Task<ResponseDTO> GetRevenueByAdmin(DateOnly startdate, DateOnly enddate);
+        Task<ResponseDTO> GetRevenueByFarm(DateOnly startdate, DateOnly enddate, Guid farmId);
+        Task<ResponseDTO> GetProfitByAdmin(DateOnly startdate, DateOnly enddate);
     }
 }
